5fcda86deff7250ba5dac1d598f738fb18fe65e5
RobertPorteles <robertporteless@gmail.com>
2026-01-28 08:51:18 -0300

    ajuste no import do Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { Login } from './components/pages/login/login';
 import { CriarUsuario } from './components/pages/criar-usuario/criar-usuario';
 import { Dashboard } from './components/pages/dashboard/dashboard';
-import { Agenda } from './services/agenda';
+
 import { authGuard } from './guards/auth-guard';
+import { Agenda } from './components/pages/agenda/agenda';
 
 export const routes: Routes = [
     {
